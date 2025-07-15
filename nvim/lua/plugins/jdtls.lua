67462c86e59c8e913a758098d3cce7c99c133d50
Mykhailo Kazymyr <mykhailokazymyr@gmail.com>
2025-07-15 09:56:20 +0000

    tmux.conf better visibility, jdtls setup to start

--- nvim/lua/plugins/jdtls.lua
+++ nvim/lua/plugins/jdtls.lua
@@ -2,6 +2,45 @@ return {
   {
     "mfussenegger/nvim-jdtls",
     opts = function(_, opts)
+      -- Detect operating system
+      local is_mac = vim.fn.has("macunix") == 1
+      local is_linux = vim.fn.has("unix") == 1 and not is_mac
+      
+      -- Set JAVA_HOME environment variable for jdtls
+      if is_linux then
+        vim.env.JAVA_HOME = "/usr/lib/jvm/java-21-openjdk-amd64"
+      elseif is_mac then
+        vim.env.JAVA_HOME = "/opt/homebrew/Cellar/openjdk@21/21.0.7/libexec/openjdk.jdk/Contents/Home"
+      end
+
+      -- Set Java runtime paths based on OS
+      local java_runtimes = {}
+      if is_mac then
+        java_runtimes = {
+          {
+            name = "JavaSE-17",
+            path = "/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home",
+            default = true,
+          },
+          {
+            name = "JavaSE-21",
+            path = "/opt/homebrew/Cellar/openjdk@21/21.0.7/libexec/openjdk.jdk/Contents/Home",
+          },
+        }
+      elseif is_linux then
+        java_runtimes = {
+          {
+            name = "JavaSE-17",
+            path = "/opt/java",
+            default = true,
+          },
+          {
+            name = "JavaSE-21",
+            path = "/usr/lib/jvm/java-21-openjdk-amd64",
+          },
+        }
+      end
+
       opts.settings = vim.tbl_deep_extend("force", opts.settings or {}, {
         java = {
           inlayHints = {
@@ -10,17 +49,7 @@ return {
             },
           },
           configuration = {
-            runtimes = {
-              {
-                name = "JavaSE-17",
-                path = "/Library/Java/JavaVirtualMachines/jdk-17.jdk/Contents/Home",
-                default = true,
-              },
-              {
-                name = "JavaSE-21",
-                path = "/opt/homebrew/Cellar/openjdk@21/21.0.7/libexec/openjdk.jdk/Contents/Home",
-              },
-            },
+            runtimes = java_runtimes,
           },
           compile = {
             nullAnalysis = {
@@ -35,7 +64,6 @@ return {
           },
         },
       })
-
       return opts
     end,
   },
